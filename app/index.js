7327c24edd90b6b8252951ce2ed0c1afc662a7e2
Admir Alicic <admir.alicic@forient.com>
2019-09-15 15:41:41 +0200

    Final animation

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
-
+import { View, Text, StyleSheet, Dimensions, TextInput } from 'react-native';
+import Svg, { Image, Circle, ClipPath } from 'react-native-svg';
 import Animated, { Easing } from 'react-native-reanimated';
 import { TapGestureHandler, State } from 'react-native-gesture-handler';
 
@@ -20,7 +20,8 @@ const {
   timing,
   clockRunning,
   interpolate,
-  Extrapolate
+  Extrapolate,
+  concat
 } = Animated;
 
 function runTiming(clock, value, dest) {
@@ -70,6 +71,18 @@ class MusicApp extends Component {
       }
     ]);
 
+    this.onCloseState = event([
+      {
+        nativeEvent: ({ state }) =>
+          block([
+            cond(
+              eq(state, State.END),
+              set(this.buttonOpacity, runTiming(new Clock(), 0, 1))
+            )
+          ])
+      }
+    ]);
+
     this.buttonY = interpolate(this.buttonOpacity, {
       inputRange: [0, 1],
       outputRange: [100, 0],
@@ -78,7 +91,31 @@ class MusicApp extends Component {
 
     this.bgY = interpolate(this.buttonOpacity, {
       inputRange: [0, 1],
-      outputRange: [-height / 3, 0],
+      outputRange: [-height / 3 - 50, 0],
+      extrapolate: Extrapolate.clamp
+    });
+
+    this.textInputZindex = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [1, -1],
+      extrapolate: Extrapolate.clamp
+    });
+
+    this.textInputY = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [1, 100],
+      extrapolate: Extrapolate.clamp
+    });
+
+    this.textInputOpacity = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [1, 0],
+      extrapolate: Extrapolate.clamp
+    });
+
+    this.rotateCross = interpolate(this.buttonOpacity, {
+      inputRange: [0, 1],
+      outputRange: [180, 360],
       extrapolate: Extrapolate.clamp
     });
   }
@@ -98,10 +135,18 @@ class MusicApp extends Component {
             transform: [{ translateY: this.bgY }]
           }}
         >
-          <Image
-            source={require('../assets/bg.jpg')}
-            style={{ flex: 1, height: null, width: null }}
-          />
+          <Svg height={height + 50} width={width}>
+            <ClipPath id='clip'>
+              <Circle r={height + 50} cx={width / 2} />
+            </ClipPath>
+            <Image
+              href={require('../assets/bg.jpg')}
+              width={width}
+              height={height + 50}
+              preserveAspectRatio='xMidWMid slice'
+              clipPath='url(#clip)'
+            />
+          </Svg>
         </Animated.View>
         <View style={{ height: height / 3, justifyContent: 'center' }}>
           <TapGestureHandler onHandlerStateChange={this.onStateChange}>
@@ -128,6 +173,43 @@ class MusicApp extends Component {
               SIGN IN WITH FACEBOOK
             </Text>
           </Animated.View>
+          <Animated.View
+            style={{
+              zIndex: this.textInputZindex,
+              opacity: this.textInputOpacity,
+              transform: [{ translateY: this.textInputY }],
+              height: height / 3,
+              ...StyleSheet.absoluteFill,
+              top: null,
+              justifyContent: 'center'
+            }}
+          >
+            <TapGestureHandler onHandlerStateChange={this.onCloseState}>
+              <Animated.View style={styles.closeButton}>
+                <Animated.Text
+                  style={{
+                    fontSize: 15,
+                    transform: [{ rotate: concat(this.rotateCross, 'deg') }]
+                  }}
+                >
+                  X
+                </Animated.Text>
+              </Animated.View>
+            </TapGestureHandler>
+            <TextInput
+              placeholder='EMAIL'
+              style={styles.textInput}
+              placeholderTextColor='black'
+            />
+            <TextInput
+              placeholder='PASSWORD'
+              style={styles.textInput}
+              placeholderTextColor='black'
+            />
+            <Animated.View style={styles.button}>
+              <Text style={{ fontSize: 20, fontWeight: 'bold' }}>SIGN IN</Text>
+            </Animated.View>
+          </Animated.View>
         </View>
       </View>
     );
@@ -148,6 +230,34 @@ const styles = StyleSheet.create({
     borderRadius: 35,
     alignItems: 'center',
     justifyContent: 'center',
-    marginVertical: 5
+    marginVertical: 5,
+    shadowOffset: { width: 2, height: 2 },
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+    elevation: 5
+  },
+  closeButton: {
+    height: 40,
+    width: 40,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: -20,
+    left: width / 2 - 20,
+    shadowOffset: { width: 2, height: 2 },
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+    elevation: 5
+  },
+  textInput: {
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 0.5,
+    marginHorizontal: 20,
+    paddingLeft: 10,
+    marginVertical: 5,
+    borderColor: 'rgba(0, 0, 0, 0.2)'
   }
 });
